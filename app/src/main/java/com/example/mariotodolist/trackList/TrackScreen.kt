a8b1a189901b3296814a5cbb985850862db38887
dmerinov <merinovecinod@gmail.com>
2025-02-22 20:45:50 +0100

    added topBar behaviour

--- app/src/main/java/com/example/mariotodolist/trackList/TrackScreen.kt
+++ app/src/main/java/com/example/mariotodolist/trackList/TrackScreen.kt
@@ -2,7 +2,6 @@ package com.example.mariotodolist.trackList
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -10,14 +9,18 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
@@ -29,37 +32,47 @@ fun TrackScreen() {
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun TrackScreen(
     uiState: TrackState
 ) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = { Text("List") },
+                colors = TopAppBarDefaults.topAppBarColors().copy(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                ),
+                scrollBehavior = scrollBehavior
+            )
+        }
     ) { innerPadding ->
-        Column(
-            Modifier
+        LazyColumn(
+            modifier = Modifier
                 .padding(innerPadding)
                 .background(MaterialTheme.colorScheme.primaryContainer)
         ) {
             if (uiState.loading) {
-                CircularProgressIndicator()
+                item { CircularProgressIndicator() }
             } else {
-                LazyColumn(
-                    modifier = Modifier.padding(16.dp)
-                ) {
-                    val list = List(50) { it + 1 }
-                    itemsIndexed(list) { index, item ->
-                        Row(
-                            modifier = Modifier.fillParentMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Text("Item ${index + 1}")
-                            Checkbox(
-                                checked = false,
-                                onCheckedChange = {},
-                            )
-                        }
+                val list = List(50) { it + 1 }
+                itemsIndexed(list) { index, item ->
+                    Row(
+                        modifier = Modifier.fillParentMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text("Item ${index + 1}")
+                        Checkbox(
+                            checked = false,
+                            onCheckedChange = {},
+                        )
                     }
                 }
             }
